c1dca86a9e2da23e09c730aadea569320c4a8eeb
BenjaminJ <benjamin@fileable.io>
2016-07-20 16:07:24 +0900

    Initial commit
    Added tests for new features

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # inquirer-select-directory
-A directory selector prompt for Inquirer.js
+
+A directory prompt for Inquirer.js [inquirer](https://github.com/SBoudrias/Inquirer.js).
+
+This project is a fork of [Inquirer-directory](https://github.com/nicksrandall/inquirer-directory) which does not limit the navigation to the current folder.
+<!--
+[![Build Status](https://travis-ci.org/nicksrandall/inquirer-directory.svg)](https://travis-ci.org/nicksrandall/inquirer-directory)
+-->
+<!--
+## Installation
+
+```bash
+npm install --save inquirer-select-directory
+```
+-->
+## Features
+
+- Support for symlinked files
+- Vim style navigation
+- Search for file with "/" key
+
+### Key Maps
+
+- Press "/" key to enter search mode.
+- Press "-" key to go up (back) a directory.
+
+## Usage
+
+
+This prompt is anonymous, meaning you can register this prompt with the type name you please:
+
+```javascript
+inquirer.registerPrompt('directory', require('inquirer-directory'));
+inquirer.prompt({
+  type: 'directory',
+  ...
+})
+```
+
+Change `directory` to whatever you might prefer.
+
+### Options
+
+Takes `type`, `name`, `message`, `basePath` properties.
+
+See [inquirer](https://github.com/SBoudrias/Inquirer.js) readme for meaning of all except **basePath**.
+
+**basePath** is the relative path from your current working directory
+
+#### Example
+
+```javascript
+inquirer.registerPrompt('directory', require('inquirer-directory'));
+inquirer.prompt([{
+  type: 'directory',
+  name: 'from',
+  message: 'Where you like to put this component?',
+  basePath: './src'
+}], function(answers) {
+  //etc
+});
+```
+<!--
+[![asciicast](https://asciinema.org/a/31651.png)](https://asciinema.org/a/31651)
+-->
+<!--
+See also [example.js](https://github.com/nicksrandall/inquierer-directory/blob/master/example.js) for a working example
+-->
+## License
+
+MIT
---
+++ test/spec/index.spec.js
@@ -0,0 +1,112 @@
+"use strict";
+
+var expect = require('chai').expect;
+var mock = require('mock-fs');
+var ReadlineStub = require('../helpers/readline');
+var Prompt = require('../../index');
+
+describe('inquirer-directory', function() {
+
+    before(function() {
+        mock({
+            'root': {
+                '.git': {},
+                'folder1': {
+                    'folder1-1': {}
+                },
+                'zfolder2': {},
+                'some.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
+                'a-symlink': mock.symlink({
+                    path: 'folder1'
+                })
+            }
+        });
+    });
+
+    after(mock.restore);
+
+    beforeEach(function() {
+        // need to clear "console after every action"
+        this.rl = new ReadlineStub();
+        this.prompt = new Prompt({
+            message: 'Choose a directory',
+            name: 'name',
+            basePath: "./root/"
+        }, this.rl);
+    });
+    afterEach(function() {
+        this.rl.output.clear();
+    });
+    it('requires a basePath', function() {
+        expect(function() {
+            new Prompt({
+                message: 'foo',
+                name: 'name',
+            });
+        }).to.throw(/basePath/);
+    });
+
+    it('should list folders', function() {
+        this.prompt.run();
+        expect(this.rl.output.__raw__).to.contain('folder1');
+        expect(this.rl.output.__raw__).to.contain('zfolder2');
+    });
+
+    it('should not contain folders starting with "." (private folders)', function() {
+        this.prompt.run();
+        expect(this.rl.output.__raw__).to.not.contain('.git');
+    });
+
+    it('should not contain files', function() {
+        this.prompt.run();
+        expect(this.rl.output.__raw__).to.not.contain('some.png');
+    });
+
+    it('should allow users to drill into folder', function() {
+        this.prompt.run();
+        this.rl.enter();
+        expect(this.rl.output.__raw__).to.contain('folder1-1');
+    });
+
+    it('should allow users to go back after drilling', function() {
+        this.prompt.run();
+        this.rl.enter();
+        expect(this.rl.output.__raw__).to.contain('go back');
+        this.rl.moveDown();
+        this.rl.moveDown();
+        this.rl.enter();
+        expect(this.rl.output.__raw__).to.contain('zfolder2');
+    });
+
+    it('should allow users to go back past basePath', function() {
+        this.prompt.run();
+        this.rl.moveDown();
+        this.rl.moveDown();
+        this.rl.moveDown();
+        this.rl.enter();
+        expect(this.rl.output.__raw__).to.contain('> root');
+        expect(this.prompt.opt.choices.realChoices[0].name).to.equal('root');
+    });
+
+    it('should not display back option in root folder', function () {
+        this.prompt.run();
+        this.rl.moveDown();
+        this.rl.moveDown();
+        this.rl.moveDown();
+        this.rl.enter();
+        this.rl.enter();
+        expect(this.rl.output.__raw__).to.contain('> root');
+        expect(this.rl.output.__raw__).to.not.contain('go back');
+        expect(this.prompt.opt.choices.realChoices.length).to.equal(2);
+    });
+    // it('should allow users to press keys to shortcut to that value', function (done) {
+    //     prompt.run(function (answer) {
+    //       expect(answer).to.equal('zfolder2');
+    //       done();
+    //     });
+    //     keyPress('z');
+    //     enter();
+    //     enter();
+    // });
+
+});
